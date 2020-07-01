58eb0be2d0cff77722e6364c3f44321cf910f478
Arpan Karki <arpankarki@hotmail.com>
2020-07-01 16:10:01 +0530

    Created Spring managed beans . Closes #22

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -18,11 +18,11 @@ public class DataLoader implements CommandLineRunner {
 	private final VetService vetService;
 	
 	
-	public DataLoader() {
-		ownerService = new OwnerServiceMap();
-		vetService = new VetServiceMap();
+	public DataLoader(OwnerService ownerService, VetService vetService) {
+		super();
+		this.ownerService = ownerService;
+		this.vetService = vetService;
 	}
-
 
 	@Override
 	public void run(String... args) throws Exception {

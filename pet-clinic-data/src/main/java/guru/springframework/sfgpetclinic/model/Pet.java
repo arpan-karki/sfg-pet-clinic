f9a66f0c61776d2f5d58f191e9238edbc827a2df
Arpan Karki <arpankarki@hotmail.com>
2020-08-10 17:53:01 +0530

    Close #38. Created Visits to Jpa entities

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -1,11 +1,15 @@
 package guru.springframework.sfgpetclinic.model;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import guru.springframework.sfgpetclinic.model.Owner;
@@ -27,9 +31,20 @@ public class Pet extends BaseEntity {
 
 	@Column(name = "part_name")
 	private String petName;
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+	private Set<Visit> visits = new HashSet<Visit>();
 
 	public String getPetName() {
 		return petName;
+	}
+
+	public Set<Visit> getVisits() {
+		return visits;
+	}
+
+	public void setVisits(Set<Visit> visits) {
+		this.visits = visits;
 	}
 
 	public void setPetName(String petName) {

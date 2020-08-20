132fb564e05b3c6fcb83c4ee8bc8512952905fe7
Karki <Arpan.Karki@dell.com>
2020-08-20 21:05:43 +0530

    Close #4. Added  JUNIT 5

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,15 +1,20 @@
 package guru.springframework.sfgpetclinic;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 //import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 class SfgPetClinicApplicationTests {
 
-/*	@Test
+	@Test
 	void contextLoads() {
 	}
 
 }
-*/
-}
+
